20e6bea756aceab0a3e7022fdd2ea727d7cafb6e
moaaz <moaz.elsayed002@gmail.com>
2022-12-11 02:31:07 +0200

    Feature:  Allow users to update their data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,13 @@
 const User = require('../model/userModel')
 const catchAsync = require('../utils/catchAsync')
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find()
 
@@ -12,6 +19,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     },
   })
 })
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    )
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email')
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  })
+})
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,11 +9,12 @@ router.post('/login', authController.login)
 
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
-router.post(
+router.patch(
   '/updatePassword',
   authController.protect,
-  authController.updatePassword,
+  authController.updatePassword
 )
+router.patch('/updateMe', authController.protect, userController.updateMe)
 
 router
   .route('/')
