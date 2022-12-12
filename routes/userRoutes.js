82039ac390c2475f81136310ebfef33fee98a004
moaaz <moaz.elsayed002@gmail.com>
2022-12-12 08:54:25 +0200

    Refactor some routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,20 +4,20 @@ const authController = require('../controllers/authController')
 
 const router = express.Router()
 
-router.get('/me', authController.protect, userController.getMe, userController.getUser)
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
-
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
-router.patch(
-  '/updatePassword',
-  authController.protect,
-  authController.updatePassword
-)
-router.patch('/updateMe', authController.protect, userController.updateMe)
-router.delete('/disableMe', authController.protect, userController.disableMe)
 
+router = router.use(authController.protect)
+
+router.get('/me', userController.getMe, userController.getUser)
+
+router.patch('/updatePassword', authController.updatePassword)
+router.patch('/updateMe', userController.updateMe)
+router.delete('/disableMe', userController.disableMe)
+
+router.use(authController.restrictTo('admin'))
 router
   .route('/')
   .get(userController.getAllUsers)
