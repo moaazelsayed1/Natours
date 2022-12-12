d13052b213af35762adf27a9e43928991dcca56c
moaaz <moaz.elsayed002@gmail.com>
2022-12-12 04:32:19 +0200

    Add Review model and basic rooutes

---
+++ model/reviewModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const reviewSchema = mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty'],
+    },
+    rating: {
+      type: Number,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+      required: [true, 'Review must has rating'],
+    },
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+)
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  })
+  next()
+})
+const Review = mongoose.model('Review', reviewSchema)
+
+module.exports = Review
