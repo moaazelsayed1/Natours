52dd4033a9a4bdcaaecfa303883336d788ba3399
moaaz <moaz.elsayed002@gmail.com>
2023-02-01 09:34:09 +0200

    Upload tours images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,60 @@
+const multer = require('multer')
+const sharp = require('sharp')
 const Tour = require('../model/tourModel')
 const AppError = require('../utils/AppError')
 const catchAsync = require('../utils/catchAsync')
 const factory = require('./handlerFactory')
+
+const multerStorage = multer.memoryStorage()
+
+// about to add resize
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true)
+  } else {
+    cb(new AppError('Not an image! Please upload only images', 400), false)
+  }
+}
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+})
+
+/* upload.single('name') */
+/* upload.array('name', 5) */
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+])
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next()
+
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`)
+
+  req.body.images = []
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`)
+
+      req.body.images.push(filename)
+    })
+  )
+  next()
+})
 
 exports.aliasTopTour = (req, res, next) => {
   req.query.limit = '5'
