f392dfe423f197020821ffa8e674a4eff13d55c7
moaaz <moaz.elsayed002@gmail.com>
2023-02-09 15:45:41 +0200

    Edit all local urls

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,7 +9,7 @@ export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
     const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+      `/api/v1/bookings/checkout-session/${tourId}`
     )
     /* console.log(session) */
 
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -7,8 +7,8 @@ export const updateSettings = async (data, type) => {
   try {
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updatePassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe'
+        ? '/api/v1/users/updatePassword'
+        : '/api/v1/users/updateMe'
 
     const res = await axios({
       method: 'PATCH',
