8414920150510d6e20e2ea69d377c133112b9160
moaaz <moaz.elsayed002@gmail.com>
2023-02-09 20:17:52 +0200

    Edit listen IP address

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose
 /* console.log(process.env) */
 
 const port = process.env.PORT || 3000
-const server = app.listen(port, '0.0.0.0' () => {
+const server = app.listen(port, '0.0.0.0', () => {
   console.log(`App running on port ${port}`)
 })
 
