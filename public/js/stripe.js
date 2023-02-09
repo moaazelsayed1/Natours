f859d1db3aa87c3772a5d2f4e995866126f3b436
moaaz <moaz.elsayed002@gmail.com>
2023-02-09 15:47:40 +0200

    Edit all local urls

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,9 +8,7 @@ const stripe = Stripe(
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `/api/v1/bookings/checkout-session/${tourId}`
-    )
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`)
     /* console.log(session) */
 
     // 2) Create checkout form + chanre credit card
